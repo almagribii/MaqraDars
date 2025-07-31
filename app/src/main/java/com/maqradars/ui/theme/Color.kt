9856676118290b1f02785db95007d98e85e90244
Almagribii <brucadalm@gmail.com>
2025-07-31 20:37:28 +0700

    update color and theme

--- app/src/main/java/com/maqradars/ui/theme/Color.kt
+++ app/src/main/java/com/maqradars/ui/theme/Color.kt
@@ -1,11 +1,15 @@
+// app/src/main/java/com/maqradars/ui/theme/Color.kt
+
 package com.maqradars.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Palet warna yang diambil dari logo
+val Primary = Color(0xFFB98D5B) // Warna emas/perunggu untuk elemen utama
+val OnPrimary = Color(0xFF2A332E) // Warna gelap untuk teks di atas Primary
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Background = Color(0xFF2A332E) // Warna latar belakang gelap
+val OnBackground = Color(0xFFB98D5B)
+
+val Surface = Color(0xFF2A332E)
+val OnSurface = Color(0xFFB98D5B)
