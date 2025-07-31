14f3ad2d5f2dd0d33898b05ed9f56f83048cc34a
Almagribii <brucadalm@gmail.com>
2025-07-31 16:58:20 +0700

    entity and Dao

--- app/src/main/java/com/maqradars/data/dao/AyatExampleDao.kt
+++ app/src/main/java/com/maqradars/data/dao/AyatExampleDao.kt
@@ -1,4 +1,30 @@
 package com.maqradars.data.dao
 
-class AyatExampleDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.maqradars.data.entity.AyatExample
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AyatExampleDao {
+    @Insert
+    suspend fun insertAyatExample(ayatExample: AyatExample) : Long
+
+    @Update
+    suspend fun updateAyatExample(ayatExample: AyatExample)
+
+    @Delete
+    suspend fun deleteAyatExample(ayatExample: AyatExample)
+
+    @Query("SELECT * FROM ayat_examples WHERE id = :ayatExampleId")
+    fun getAyatById(ayatExampleId: Long) : Flow <AyatExample?>
+
+    @Query("SELECT * FROM ayat_examples WHERE maqam_id = :maqamId ORDER BY surah_number")
+    fun getAyatExamplesByMaqamId(maqamId : Long) : Flow<List<AyatExample>>
+
+    @Query("DELETE FROM ayat_examples WHERE id = :ayatExampleId")
+    suspend fun deleteAyatExampleById(ayatExampleId: Long)
 }
--- app/src/main/java/com/maqradars/data/entity/AyatExample.kt
+++ app/src/main/java/com/maqradars/data/entity/AyatExample.kt
@@ -1,4 +1,39 @@
 package com.maqradars.data.entity
 
-class AyatExample {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "ayat_examples",
+    foreignKeys = [
+        ForeignKey(
+            entity = Maqam::class,
+            parentColumns = ["id"],
+            childColumns = ["maqam_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
+data class AyatExample (
+    @PrimaryKey(autoGenerate = true)
+    val id : Long = 0,
+
+    @ColumnInfo(name = "maqam_id")
+    val maqamId: Long,
+
+    @ColumnInfo(name = "surah_number")
+    val surahNumber: Int,
+
+    @ColumnInfo(name = "ayat_number")
+    val ayatNumber: Int,
+
+    @ColumnInfo(name = "arabic_text")
+    val arabicText: String,
+
+    @ColumnInfo(name = "translation_text")
+    val translationText: String?,
+
+    @ColumnInfo(name = "audio_path_ayat_example")
+    val audioPathAyatExample: String
+)
